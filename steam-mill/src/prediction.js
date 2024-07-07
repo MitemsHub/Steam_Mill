79b2070ee30e83199d3f1fc7c9d1b5034b8eb352
Emmanuel C. Mitti <98887395+MitemsHub@users.noreply.github.com>
2024-07-07 22:39:10 +0000

    Add data collection to Failure Prediction

--- steam-mill/src/prediction.js
+++ steam-mill/src/prediction.js
@@ -4,6 +4,10 @@ class FailurePredictor {
     constructor() {
       this.history = [];
     }
+  
+    addData(engineId, data) {
+      this.history.push({ engineId, data });
+    }
   }
   
   module.exports = FailurePredictor;
