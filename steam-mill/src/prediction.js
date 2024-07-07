b6ba51ca04aa95be3b0e512be4d36db170ab30fb
Emmanuel C. Mitti <98887395+MitemsHub@users.noreply.github.com>
2024-07-07 22:40:05 +0000

    Add simple failure prediction logic

--- steam-mill/src/prediction.js
+++ steam-mill/src/prediction.js
@@ -8,6 +8,15 @@ class FailurePredictor {
     addData(engineId, data) {
       this.history.push({ engineId, data });
     }
+  
+    predictFailure(engineId) {
+      const engineData = this.history.filter(h => h.engineId === engineId);
+      // Simple prediction logic (to be enhanced)
+      if (engineData.length > 5) {
+        return 'High risk of failure';
+      }
+      return 'Low risk of failure';
+    }
   }
   
   module.exports = FailurePredictor;
